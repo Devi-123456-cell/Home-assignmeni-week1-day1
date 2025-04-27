ddd9e1bba4133fd1255124d266b8586dbebe52e9
Devi-123456-cell <deevivasu@gmail.com>
2025-04-27 09:02:05 +0800

    Add files via upload

---
+++ chrome.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class chrome {
+
+	public static void main(String[] args) {
+		
+		
+		//Integer
+		
+		long year=2008;
+			
+		System.out.println(year);
+		
+		boolean good=false;
+				System.err.println(good);
+				
+				char name='c';
+				System.err.println(name);
+				float dec=91.0f;
+				System.err.println(dec);
+				
+				//Non primitive datatypes
+				String org="Google";
+				System.err.println(org);
+
+	}
+
+}
